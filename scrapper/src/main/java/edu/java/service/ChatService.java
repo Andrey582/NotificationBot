05b91a193ff4fcd6c1d176975b9994890a0ff528
Andrey Ganyushkin <gandrey0606@mail.ru>
2024-03-21 19:54:00 +0300

    add some changes

--- scrapper/src/main/java/edu/java/service/ChatService.java
+++ scrapper/src/main/java/edu/java/service/ChatService.java
@@ -1,8 +1,12 @@
 package edu.java.service;
 
+import org.springframework.transaction.annotation.Transactional;
+
 public interface ChatService {
 
+    @Transactional
     void register(Long chatId);
 
+    @Transactional
     void unregister(Long chatId);
 }
--- scrapper/src/main/java/edu/java/service/LinkService.java
+++ scrapper/src/main/java/edu/java/service/LinkService.java
@@ -3,12 +3,16 @@ package edu.java.service;
 import edu.java.dto.request.LinkRequestDto;
 import edu.java.dto.response.LinkResponseDto;
 import edu.java.dto.response.ListLinkResponseDto;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface LinkService {
 
+    @Transactional
     LinkResponseDto create(Long chatId, LinkRequestDto body);
 
+    @Transactional
     LinkResponseDto delete(Long chatId, LinkRequestDto body);
 
+    @Transactional(readOnly = trueg)
     ListLinkResponseDto getAll(Long chatId);
 }
